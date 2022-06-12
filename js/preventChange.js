65291eeedb5224ca1af98d4de4d95f07d1a7204d
cph101 <cph101@icloud.com>
2022-06-12 10:43:45 +0100

    Added rightful copyright
    // © Copyright 2022 www.cph101.com

--- js/preventChange.js
+++ js/preventChange.js
@@ -1,3 +1,8 @@
+// © Copyright 2022 www.cph101.com
+// This file was originally made by cph101
+// Do not rebrand if you are distributing it
+// © Copyright 2022 www.cph101.com
+
 const originalContents = document.body.innerHTML;
 const originalHead = document.head.innerHTML;
 
