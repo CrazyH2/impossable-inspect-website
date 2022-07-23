871e4de3273cfb0f204e1fcf3c66d97fab61eebd
Crazy H <87752773+CrazyH2@users.noreply.github.com>
2022-07-23 16:27:12 +0100

    Update preventChange.js

--- js/preventChange.js
+++ js/preventChange.js
@@ -1,18 +1,21 @@
 const originalContents = document.body.innerHTML;
 const originalHead = document.head.innerHTML;
 
-import {show_hide} from ‘./preventCopy.js’
-
 function checkEdit() {
-  if (document.body.innerHTML == originalContents) {
-    // Everything is alright
+  import {show_hide} from ‘./preventCopy.js’
+  if (show_hide == "stop")
+    // Everything is hidden
   } else {
-    document.body.innerHTML = originalContents;
-  }
-  if (document.head.innerHTML == originalHead) {
-    // Everything is alright
-  } else {
-    document.head.innerHTML = originalHead;
+    if (document.body.innerHTML == originalContents) {
+      // Everything is alright
+    } else {
+      document.body.innerHTML = originalContents;
+    }
+    if (document.head.innerHTML == originalHead) {
+      // Everything is alright
+    } else {
+      document.head.innerHTML = originalHead;
+    }
   }
 }
 
