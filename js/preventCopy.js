4ebd8c0ab7098418121ed730d7810829328c0d2c
Crazy H <87752773+CrazyH2@users.noreply.github.com>
2022-07-23 16:23:56 +0100

    Update preventCopy.js

--- js/preventCopy.js
+++ js/preventCopy.js
@@ -4,11 +4,15 @@ var elementWithHiddenContent = document.querySelector("#element-to-hide");
 var innerHtml = elementWithHiddenContent.innerHTML;
 
 element.__defineGetter__("id", function() {
+    var show_hide = "stop";
+    export var show_hide;
     currentInnerHtml = "";
 });
 
 setInterval(function() {
     currentInnerHtml = innerHtml;
+    var show_hide = "start";
+    export var show_hide;
     console.log(element);
     console.clear();
     elementWithHiddenContent.innerHTML = currentInnerHtml;
