68eff87274b3d9628bdfab01b69926b3574eb3d8
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2022-10-25 13:16:23 +0100

    Update preventCopy.js

--- js/preventCopy.js
+++ js/preventCopy.js
@@ -1,6 +1,6 @@
 var currentInnerHtml;
 var element = new Image();
-var elementWithHiddenContent = document.querySelector("#element-to-hide");
+var elementWithHiddenContent = document.querySelector("body");
 var innerHtml = elementWithHiddenContent.innerHTML;
 
 element.__defineGetter__("id", function() {
